8893dcdeaa87b5754b1da09fe6297c71e187fd0e
Mirco Kraenz <kraenzmirco@gmail.com>
2022-08-18 23:03:02 +0200

    fix: add missing pixel + autogenerate midori and pink

---
+++ cat-color-replacing.js
@@ -0,0 +1,49 @@
+// @ts-nocheck
+
+const replaceColor = require("replace-color");
+
+const main = async () => {
+    // light blue #3f3f74 -> light green #4b692f
+    const midori1 = await replaceColor({
+        image: "./assets/images/ao.png",
+        colors: {
+            type: "hex",
+            targetColor: "#5FCDE4", // current color we want to replace
+            replaceColor: "#99E550", // new color to replace with
+        },
+    });
+    // dark blue #3f3f74 -> dark green #4b692f
+    const midori2 = await replaceColor({
+        image: midori1,
+        colors: {
+            type: "hex",
+            targetColor: "#3f3f74", // current color we want to replace
+            replaceColor: "#4b692f", // new color to replace with
+        },
+    });
+    midori2.write("./assets/images/midori.png", (err) => {
+        if (err) return console.log(err);
+    });
+
+    const pink1 = await replaceColor({
+        image: "./assets/images/ao.png",
+        colors: {
+            type: "hex",
+            targetColor: "#5FCDE4", // current color we want to replace
+            replaceColor: "#ffabb5", // new color to replace with
+        },
+    });
+    const pink2 = await replaceColor({
+        image: pink1,
+        colors: {
+            type: "hex",
+            targetColor: "#3f3f74", // current color we want to replace
+            replaceColor: "#94656b", // new color to replace with
+        },
+    });
+    pink2.write("./assets/images/pink.png", (err) => {
+        if (err) return console.log(err);
+    });
+};
+
+main().catch((e) => console.error(e));
